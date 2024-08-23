5c2196aab50abcf545d312b05261d9babb79fd27
rob <robdelacruz@gmail.com>
2024-08-24 00:35:47 +0800

    various

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,33 @@
 #include "wx/wx.h"
+#include "wx/window.h"
 #include "wx/colour.h"
 #include "wx/listctrl.h"
 #include "clib.h"
 #include "Db.h"
 
+enum {
+    ID_EXPENSES_PANEL = wxID_HIGHEST,
+    ID_EXPENSES_YEAR,
+    ID_EXPENSES_LISTVIEW,
+    ID_COUNT
+};
+
 class ExpFrame : public wxFrame {
-public:
+private:
+    sqlite3 *db;
     int selYear;
     int selMonth;
 
+    wxDECLARE_EVENT_TABLE();
+
+public:
     ExpFrame(const wxString& title);
     ~ExpFrame();
+
+    void CreateControls();
+    void RefreshControls();
+
+    void OnSize(wxSizeEvent& e);
 };
 
 class ExpApp : public wxApp {
@@ -18,6 +35,10 @@ public:
     virtual bool OnInit();
 };
 wxIMPLEMENT_APP(ExpApp);
+
+wxBEGIN_EVENT_TABLE(ExpFrame, wxFrame)
+    EVT_SIZE(ExpFrame::OnSize)
+wxEND_EVENT_TABLE()
 
 bool ExpApp::OnInit() {
     ExpFrame *w = new ExpFrame("Expense Buddy GUI");
@@ -31,25 +52,99 @@ ExpFrame::ExpFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDef
     font.SetPointSize(10);
     SetFont(font);
 
-    sqlite3 *db;
     int z = open_expense_file("rob.db", &db);
     if (z != 0)
         quit("Expense file not found.");
 
-    wxPanel *pnl = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxDefaultSize);
-    wxListView *lv = new wxListView(pnl, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxLC_REPORT);
+    CreateControls();
+    RefreshControls();
+
+}
+ExpFrame::~ExpFrame() {
+}
+
+static void SetColorsFromParent(wxWindow *w) {
+    wxWindow *parent = w->GetParent();
+    if (parent == NULL)
+        return;
+
+    w->SetBackgroundColour(parent->GetBackgroundColour());
+    w->SetForegroundColour(parent->GetForegroundColour());
+}
+
+static void SetFontSize(wxWindow *w, int pointSize) {
+    wxFont font = w->GetFont();
+    font.SetPointSize(pointSize);
+    w->SetFont(font);
+}
+
+static void FitListViewColumnWidths(wxListView *lv) {
+    int ncols = lv->GetColumnCount();
+    if (ncols == 0)
+        return;
+
+    int w = lv->GetClientSize().GetWidth();
+    for (int i=0; i < ncols-1; i++)
+        w -= lv->GetColumnWidth(i);
+
+    lv->SetColumnWidth(ncols-1, w);
+}
+
+void ExpFrame::CreateControls() {
+    wxPanel *pnl = new wxPanel(this, ID_EXPENSES_PANEL, wxDefaultPosition, wxDefaultSize);
+
+    wxArrayString years;
+    years.Add("2024");
+    years.Add("2023");
+    years.Add("2022");
+    years.Add("2021");
+    years.Add("2020");
+    years.Add("2019");
+    years.Add("2018");
+    years.Add("2017");
+    years.Add("2016");
+    years.Add("2015");
+    wxChoice *chYears = new wxChoice(pnl, ID_EXPENSES_YEAR, wxDefaultPosition, wxDefaultSize, years);
+    chYears->SetSelection(0);
+    SetFontSize(chYears, 8);
+
+    //wxListView *lv = new wxListView(pnl, ID_EXPENSES_LISTVIEW, wxDefaultPosition, wxDefaultSize, wxLC_REPORT);
+    //wxListView *lv = new wxListView(pnl, ID_EXPENSES_LISTVIEW, wxDefaultPosition, wxDefaultSize, wxLC_REPORT|wxLC_NO_HEADER);
+    wxListView *lv = new wxListView(pnl, ID_EXPENSES_LISTVIEW, wxDefaultPosition, wxDefaultSize);
     lv->AppendColumn("Date");
     lv->AppendColumn("Description");
     lv->AppendColumn("Amount");
     lv->AppendColumn("Category");
-//    lv->SetColumnWidth(0, wxLIST_AUTOSIZE_USEHEADER);
-//    lv->SetColumnWidth(1, 100);
-//    lv->SetColumnWidth(2, wxLIST_AUTOSIZE_USEHEADER);
-//    lv->SetColumnWidth(3, wxLIST_AUTOSIZE_USEHEADER);
+    lv->SetColumnWidth(0, wxLIST_AUTOSIZE_USEHEADER);
+    lv->SetColumnWidth(1, 150);
+    lv->SetColumnWidth(2, wxLIST_AUTOSIZE_USEHEADER);
+    lv->SetColumnWidth(3, wxLIST_AUTOSIZE_USEHEADER);
+
+    wxBoxSizer *hb = new wxBoxSizer(wxHORIZONTAL);
+    hb->Add(chYears, 0, wxEXPAND, 0);
+
+    wxBoxSizer *vb = new wxBoxSizer(wxVERTICAL);
+    vb->Add(hb, 0, wxEXPAND, 0);
+    vb->Add(lv, 1, wxEXPAND, 0);
+    pnl->SetSizer(vb);
+
+    vb = new wxBoxSizer(wxVERTICAL);
+    vb->Add(pnl, 1, wxEXPAND, 0);
+    SetSizer(vb);
+
+    SetBackgroundColour(wxColour(0x1f,0x29,0x37));
+    SetForegroundColour(wxColour(0xe5,0xe7,0xeb));
+    SetColorsFromParent(pnl);
+    SetColorsFromParent(chYears);
+    SetColorsFromParent(lv);
+}
+
+void ExpFrame::RefreshControls() {
+    wxListView *lv = (wxListView *) wxWindow::FindWindowById(ID_EXPENSES_LISTVIEW, this);
+    assert(lv != NULL);
 
     vector<Expense> xps;
     SelectExpensesByMonth(db, 2024, 6, xps);
-
     for (int i=0; i < (int) xps.size(); i++) {
         Expense xp = xps[i];
         lv->InsertItem(i, wxDateTime(xp.date).Format("%m-%d"));
@@ -57,31 +152,19 @@ ExpFrame::ExpFrame(const wxString& title) : wxFrame(NULL, wxID_ANY, title, wxDef
         lv->SetItem(i, 2, wxString::Format("%'9.2f", xp.amt));
         lv->SetItem(i, 3, xp.catname);
     }
-
     lv->SetColumnWidth(0, -1);
-    lv->SetColumnWidth(1, 150);
+    //lv->SetColumnWidth(1, 150);
+    lv->SetColumnWidth(1, -1);
     lv->SetColumnWidth(2, -1);
     lv->SetColumnWidth(3, -1);
-
-    wxBoxSizer *vbox = new wxBoxSizer(wxVERTICAL);
-    vbox->Add(lv, 1, wxEXPAND | wxALL, 0);
-    pnl->SetSizer(vbox);
-
-    vbox = new wxBoxSizer(wxVERTICAL);
-    vbox->Add(pnl, 1, wxEXPAND, 0);
-    SetSizer(vbox);
-
-    SetBackgroundColour(wxColour(0x1f,0x29,0x37));
-    SetForegroundColour(wxColour(0xe5,0xe7,0xeb));
-
-    pnl->SetBackgroundColour(*wxYELLOW);
-    pnl->SetForegroundColour(*wxGREEN);
-    pnl->SetBackgroundColour(wxColour(0x1f,0x29,0x37));
-    pnl->SetForegroundColour(wxColour(0xe5,0xe7,0xeb));
-
-    lv->SetBackgroundColour(pnl->GetBackgroundColour());
-    lv->SetForegroundColour(pnl->GetForegroundColour());
 }
-ExpFrame::~ExpFrame() {
+
+void ExpFrame::OnSize(wxSizeEvent& e) {
+    printf("OnSize()\n");
+    wxListView *lv = (wxListView *) wxWindow::FindWindowById(ID_EXPENSES_LISTVIEW, this);
+    assert(lv != NULL);
+    FitListViewColumnWidths(lv);
+
+    e.Skip(true);
 }
 
